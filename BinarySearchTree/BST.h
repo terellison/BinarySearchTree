67650e0f53a962479cdecf8cf514b9e4f80eeb86
terellison <ellisonterry4@gmail.com>
2023-09-13 18:45:43 -0400

    moved BST back into an h file moved demo code into main.cpp

--- BinarySearchTree/BST.h
+++ BinarySearchTree/BST.h
@@ -1,10 +1,5 @@
 #include <iostream>
 #include <string>
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
-
 
 // Templated binary search tree
 template<typename Type>
@@ -416,38 +411,3 @@ private:
 		}
 	}
 };
-
-int main()
-{
-	
-
-	BST<int>* searchTree = new BST<int>();
-
-	searchTree->Push(50);
-	searchTree->Push(25);
-	searchTree->Push(75);
-	searchTree->Push(10);
-	searchTree->Push(35);
-	searchTree->Push(65);
-	searchTree->Push(100);
-	searchTree->Push(15);
-	searchTree->Push(40);
-	searchTree->Push(60);
-	searchTree->Push(80);
-
-	int searchValue = 15;
-
-	std::cout << "Contains " << searchValue << ": " << (searchTree->Contains(15) ? "true" : "false") << std::endl;
-
-	std::cout << searchTree->InOrder() << std::endl;
-
-	std::cout << searchTree->Remove(65);
-
-	std::cout << searchTree->InOrder() << std::endl;
-
-	delete searchTree;
-
-	_CrtDumpMemoryLeaks();
-
-	return 0;
-}
